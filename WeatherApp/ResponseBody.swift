30d4ebb76ae6c2a16a921cc3cf217dd0b04571bb
bluesky15 <lalit143.er@gmail.com>
2024-05-15 19:35:32 +0530

    able to get the weather data

---
+++ WeatherApp/ResponseBody.swift
@@ -0,0 +1,8 @@
+//
+//  ResponseBody.swift
+//  WeatherApp
+//
+//  Created by LALIT KUMAR BEHERA on 15/05/24.
+//
+
+import Foundation
---
+++ WeatherApp/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by LALIT KUMAR BEHERA on 15/05/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
